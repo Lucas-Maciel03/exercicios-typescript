61506655ed0508c1b25c8072f33c0855f028288f
Lucas Maciel <farias.maciel@outlook.com>
2023-11-16 22:52:52 -0400

    desafio 1

---
+++ 1_INTRO/desafio.js
@@ -0,0 +1,7 @@
+var num1 = 30;
+var num2 = 12;
+function calculo(x, y) {
+    console.log(x + y);
+}
+calculo(num1, num2);
+calculo(3, 5);
---
+++ 1_INTRO/desafio.ts
@@ -0,0 +1,9 @@
+const num1 = 30
+const num2 = 12
+
+function calculo(x: number, y: number){
+    console.log(x + y)
+}
+
+calculo(num1, num2)
+calculo(3, 5)
