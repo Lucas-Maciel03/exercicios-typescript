ee6123e48c201440ee00f4379772e7ecf0af3313
Lucas Maciel <farias.maciel@outlook.com>
2023-11-17 21:59:20 -0400

    fundamentos tipos de dados primitivos

---
+++ 2_FUNDAMENTOS/index.js
@@ -0,0 +1,21 @@
+// 1- number
+var x = 20;
+console.log(x);
+var y = 3.142345323;
+console.log(typeof y);
+console.log(y);
+console.log(y.toPrecision(3));
+//2 - string
+var firstName = "Lucas";
+console.log(firstName.toUpperCase());
+var fullName;
+var lastName = "Maciel";
+fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(typeof fullName);
+//3 - boolean
+var a = false;
+console.log(a);
+console.log(typeof a);
+a = true;
+console.log(a);
---
+++ 2_FUNDAMENTOS/index.ts
@@ -0,0 +1,36 @@
+// 1- number
+let x: number = 20
+
+console.log(x)
+
+let y: number = 3.142345323
+
+console.log(typeof y)
+
+console.log(y)
+
+console.log(y.toPrecision(3))
+
+//2 - string
+const firstName: string = "Lucas"
+
+console.log(firstName.toUpperCase())
+
+let fullName: string
+
+const lastName: string = "Maciel"
+
+fullName = firstName + " " + lastName
+
+console.log(fullName)
+
+console.log(typeof fullName)
+
+//3 - boolean
+let a: boolean = false
+
+console.log(a)
+console.log(typeof a)
+
+a = true
+console.log(a)
