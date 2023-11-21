9d9a84a447171140fcc89015de30079f3c8e34dc
Lucas Maciel <farias.maciel@outlook.com>
2023-11-20 21:01:51 -0400

    formas de declarar array

---
+++ 3_TIPOS_DE_DADOS/index.js
@@ -0,0 +1,12 @@
+"use strict";
+// 1 - arrays[]
+let numbers = [1, 5, 6, 8];
+numbers.push(3);
+console.log(numbers[2]);
+const nomes = ["Maria", "Joao", "André"];
+nomes.push("Ale");
+console.log(nomes);
+//outra sintaxe Array (menos utilizada)
+const nums = [1, 3, 4];
+nums.push(9);
+console.log(nums);
---
+++ 3_TIPOS_DE_DADOS/index.ts
@@ -0,0 +1,17 @@
+// 1 - arrays[]
+let numbers: number[] = [1, 5, 6, 8]
+numbers.push(3)
+
+console.log(numbers[2])
+
+const nomes: string[] = ["Maria", "Joao", "André"]
+
+nomes.push("Ale")
+console.log(nomes)
+
+//outra sintaxe Array (menos utilizada)
+const nums: Array<number> = [1, 3, 4]
+
+nums.push(9)
+console.log(nums)
+
