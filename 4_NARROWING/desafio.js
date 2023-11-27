780c8d710d0e1aad260e2ea281ef611fd0f54701
Lucas Maciel <farias.maciel@outlook.com>
2023-11-26 22:37:43 -0400

    desafio 3

---
+++ 4_NARROWING/desafio.js
@@ -0,0 +1,34 @@
+"use strict";
+function review(note) {
+    if (note) {
+        if (note === 1) {
+            console.log(`Avaliação nota ${note}`);
+        }
+        else if (note === 2) {
+            console.log(`Avaliação nota ${note}`);
+        }
+        else if (note === 3) {
+            console.log(`Avaliação nota ${note}`);
+        }
+        else if (note === 4) {
+            console.log(`Avaliação nota ${note}`);
+        }
+        else if (note === 5) {
+            console.log(`Avaliação nota ${note}`);
+        }
+        else {
+            console.log("Informe uma avaliação entre as notas 1 e 5");
+        }
+    }
+    else {
+        console.log("Informe uma avaliação valida!");
+    }
+}
+review(false);
+review(8);
+review(-25);
+review(1);
+review(2);
+review(3);
+review(4);
+review(5);
