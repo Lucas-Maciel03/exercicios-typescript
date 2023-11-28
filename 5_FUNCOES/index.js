17dec18da65d0ce3b1deae581effcdb99b728571
Lucas Maciel <farias.maciel@outlook.com>
2023-11-27 20:52:48 -0400

    funcao sem retorno

---
+++ 5_FUNCOES/index.js
@@ -0,0 +1,7 @@
+"use strict";
+//1 - função sem retorno (void)
+function withoutReturn() {
+    console.log('Essa função não tem retorno!');
+    //return 2
+}
+withoutReturn();
---
+++ 5_FUNCOES/index.ts
@@ -0,0 +1,7 @@
+//1 - função sem retorno (void)
+function withoutReturn():void{
+    console.log('Essa função não tem retorno!')
+    //return 2
+}
+
+withoutReturn()
