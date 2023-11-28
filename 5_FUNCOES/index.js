3c0f2576df4d39a59a1af1ab0029638239f1bdd7
Lucas Maciel <farias.maciel@outlook.com>
2023-11-27 21:03:29 -0400

    callback como argumento

--- 5_FUNCOES/index.js
+++ 5_FUNCOES/index.js
@@ -5,3 +5,17 @@ function withoutReturn() {
     //return 2
 }
 withoutReturn();
+//2 - callback como argumento
+function greeting(name) {
+    return `Olá, ${name}`;
+}
+//f: (name: string) - é a callback
+//=> string - é o tipo de retorno da função
+function preGreeting(f, userName) {
+    console.log('Preparando chamada!');
+    const greet = f(userName);
+    console.log(greet);
+}
+preGreeting(greeting, "Lucas");
+preGreeting(greeting, "cas");
+preGreeting(greeting, "dds");
--- 5_FUNCOES/index.ts
+++ 5_FUNCOES/index.ts
@@ -5,3 +5,22 @@ function withoutReturn():void{
 }
 
 withoutReturn()
+
+//2 - callback como argumento
+function greeting(name: string): string{
+    return `Olá, ${name}`
+}
+
+//f: (name: string) - é a callback
+//=> string - é o tipo de retorno da função
+function preGreeting(f: (name: string) => string, userName: string){
+    console.log('Preparando chamada!')
+
+    const greet = f(userName)
+
+    console.log(greet)
+}
+
+preGreeting(greeting, "Lucas")
+preGreeting(greeting, "cas")
+preGreeting(greeting, "dds")
